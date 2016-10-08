a1dca46925c3c05419ba41b464697ac5c99646bb
Jerson Martínez (Side Master) <sidemasterdelfuturo@gmail.com>
2016-10-08 03:11:20 -0600

    Adding comments

--- app/config/install/ic.SaveFileConfig.php
+++ app/config/install/ic.SaveFileConfig.php
@@ -1,18 +1,32 @@
 <?php
-	/*Se incluye el fichero que contiene las constantes, listas para ser llamadas*/
+	/*Se incluye el fichero que contiene las constantes, 
+	listas para ser llamadas*/
 	include ($_SERVER['DOCUMENT_ROOT']."/app/core/ic.const.php");
 
-	/*Se incluye una clase que contiene los métodos necesarios para realizar las operaciones de configuración*/
+	/*Se incluye una clase que contiene los métodos 
+	necesarios para realizar las operaciones 
+	de configuración*/
 	include (PD_CONTROLLER_PHP."/ic.config.class.php");
 	
+	/*Se crea un objeto Config, 
+	instanciando la clase ConfigFile*/
 	$Config = new ConfigFile();
+	
+	/*Se crea el fichero, se le pasa por 
+	parámetros la ruta del fichero y los datos.*/
 	$Config->CreateFile(PF_CONFIG, $_POST);
+
+	/*Se confirma el fichero en dicha ruta*/
 	$Config->ConfirmFile(PF_CONFIG);
 	
-	/*Se hace la conexión al servidor y se crean las tablas en la base de datos*/
+	/*Creando la conexión al servidor*/
 	include (PF_CONNECT_SERVER);
+
+	/*Creando tablas en la base de datos seleccionada*/
 	include (PF_INSTALLDB);
 
+	/*Con respecto a la conexión y creación de tablas
+	me devuelven la variable error, por si hay error.*/
 	if ($error == false)
 		echo "OK";
 	else if ($error == true)
--- app/controller/php/ic.config.class.php
+++ app/controller/php/ic.config.class.php
@@ -2,23 +2,35 @@
 	class ConfigFile {
 
 		public function CreateFile($fn, $data){
+			/*Se crea el fichero*/
 			touch($fn);
+
+			/*Se le asignan permisos al fichero
+			0-rwx-r-x-r-x*/
 			chmod($fn, 0744);
 
+			/*Puntero de tipo fichero, $MyFile apunta al fichero*/
 			$MyFile = @fopen($fn, "w");
+
+			/*Se tiene un modo escritura, se escriben
+			los datos que me enviaron del formulario.*/
 			fwrite($MyFile, $data['host'].PHP_EOL);
 			fwrite($MyFile, $data['username'].PHP_EOL);
 			fwrite($MyFile, $data['password'].PHP_EOL);
 			fwrite($MyFile, $data['database'].PHP_EOL);
 			fwrite($MyFile, $data['prefix']);
 
+			/*Se cierra el fichero*/
 			fclose($MyFile);
 
 			return;
 		}
 
 		public function ConfirmFile($fn){
+			/*Se verifica que el fichero exista*/
 			if (file_exists($fn)){
+				/*Si no se puede leer o no se puede escribir
+				entonces que intenten crear el fichero nuevamente.*/
 				if (!is_readable($fn) && !is_writable($fn)){
 					$this->CreateFile($fn);
 				} else {
@@ -27,6 +39,9 @@
 			} else {
 				$this->CreateFile();
 			}
+
+			/*Si todo va bien, entonces $error tiene un valor falso,
+			indicando que no hay error.*/
 		}
 
 	}
