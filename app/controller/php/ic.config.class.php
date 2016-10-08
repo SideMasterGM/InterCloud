927a5db74b9c3ab0ccd6ac32a7f6c25d5ff7e4ff
Jerson Martínez (Side Master) <sidemasterdelfuturo@gmail.com>
2016-10-08 17:33:51 -0600

    Comment methods of the class ConfigFile of the file ic.config.class.php

--- app/controller/php/ic.config.class.php
+++ app/controller/php/ic.config.class.php
@@ -47,26 +47,42 @@
 		}
 
 		public function CFC($fn){
+			#CFC: Create File Config.
+
+			#Crear el fichero $fn.
 			@touch($fn);
+
+			#Asignar permisos al fichero $fn.
 			@chmod($fn, 0744);
 
+			#Se apertura el fichero en modo escritura, de esto sale un puntero.
 			$rf = @fopen($fn, "w");
+
+			#Se escribe los datos de configuración por defecto en el fichero $fn.
 			fwrite($rf, "Host".PHP_EOL);
 			fwrite($rf, "Nombre de usuario".PHP_EOL);
 			fwrite($rf, "Contraseña".PHP_EOL);
 			fwrite($rf, "Nombre de la base de datos".PHP_EOL);
 			fwrite($rf, "Prefijo");
 
+			#Se cierra el fichero $fn.
 			fclose($rf);
 
-			return;
+			return; #Retornamos nada.
 		}
 
 		public function getIpAddr(){
+			#Verificación IP de la variable global de servidor HTTP_CLIENT_IP
+			#En caso de no estar vacía, retorna esa dirección IP.
+
+			#En caso que HTTP_X_FORWARDED_FOR no esté vacía se retorna la dirección IP.
+
 	        if (!empty(@$_SERVER['HTTP_CLIENT_IP']))
 	            return @$_SERVER['HTTP_CLIENT_IP'];
 	        else if (!empty(@$_SERVER['HTTP_X_FORWARDED_FOR']))
 	            return @$_SERVER['HTTP_X_FORWARDED_FOR'];
+
+	        #Si ha llegado hasta acá, re retorna la dirección IP que contiene REMOTE_ADDR.
 	        return @$_SERVER['REMOTE_ADDR'];
 	    }
 
